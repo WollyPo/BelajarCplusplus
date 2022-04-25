4c0f9c594353a50cb4e1d982040050f4aae70537
WollyPo <willopo9or7@gmail.com>
2022-04-25 18:41:29 +0700

    Day 32

---
+++ bjarne/2-1 Types and Declarations/program/32/types.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+float x; // x is a floating-point var iable
+int y = 7; // y is an integer var iable with the initial value 7
+float f(int); // f is a function taking an argument of type int and returning a floating-point number
+int main(){
+    x = y + f(2);
+    return 0;
+}
